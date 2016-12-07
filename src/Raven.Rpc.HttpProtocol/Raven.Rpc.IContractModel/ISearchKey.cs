f5929c87741cb8eed26f527e321a758e4f2c54a8
LiangYi <liangyi@mallcoo.cn>
2016-12-07 16:53:59 +0800

    DTO add ISearchKey

---
+++ src/Raven.Rpc.HttpProtocol/Raven.Rpc.IContractModel/ISearchKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raven.Rpc.IContractModel
+{
+    /// <summary>
+    /// ISearchKey
+    /// </summary>
+    public interface ISearchKey
+    {
+        /// <summary>
+        /// GetSearchKey
+        /// </summary>
+        /// <returns></returns>
+        string GetSearchKey();
+    }
+}
