c134f4dc5210c5a774b22904c2655c4ec6ec7fa3
indifer <indifer@126.com>
2017-03-15 17:57:34 +0800

    up

---
+++ src/Raven.Rpc.HttpProtocol.Test/Raven.Rpc.HttpProtocol.AspCore/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Raven.WebAPIConsoleApp.Models;
+
+namespace Raven.Rpc.HttpProtocol.AspCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet]
+        public ResponseModel<User> Get()
+        {
+            return new ResponseModel<User>() { Data = new User { Name = "ResponseModel-Get" } };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
